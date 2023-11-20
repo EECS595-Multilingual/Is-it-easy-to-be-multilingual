0a44956b0d14eb8d010900cc11bf1a8086bcce67
Karan Anand <58492510+karananand01@users.noreply.github.com>
2023-11-20 12:01:45 -0500

    lexical similarity

--- lexical_sim.py
+++ lexical_sim.py
@@ -6,7 +6,7 @@ lang2file = {
     'fi' : 'fi-train',
     'ar' : 'ar-train',
     'bn' : 'bn-train',
-    'id' : 'in-train',
+    'id' : 'id-train',
     'ko' : 'ko-train',
     'ru' : 'ru-train',
     'sw' : 'sw-train',
@@ -24,29 +24,28 @@ def read_data(path):
     with open(path, 'r') as f:
         for line in f:
             if line.strip()=="":
-                sentences+=sentence
-                sentence=""
+                sentences.append(" ".join(sentence))
+                sentence=[]
                 continue
             words = line.strip().split()
-            sentence+=words[0].split(":")[1]
+            sentence.append(words[0].split(":")[1])
 
     return sentences
 
 def calculate_njsd(p, q):
+    min_len = min(len(p), len(q))
+    p = p[:min_len]
+    q = q[:min_len]
     # Calculate Jensen-Shannon Divergence
     jsd = jensenshannon(p, q)
 
-    # Calculate entropy of the average distribution
-    m = 0.5 * (p + q)
-    entropy_m = -np.sum(m * np.log2(m))
-
     # Normalize the Jensen-Shannon Divergence
-    njsd = jsd / np.sqrt(2 * (1 - entropy_m))
+    njsd = jsd / np.sqrt(2)
 
     return njsd
 
 def convert_ngram_prob(text,n):
-    ngrams = [text[i:i+n] for i in range(len(text)-n+1)]
+    ngrams = [tuple(text[i:i+n]) for i in range(len(text)-n+1)]
     ngram_counts = {}
     for gram in ngrams:
         ngram_counts[gram] = ngram_counts.get(gram, 0) + 1
@@ -59,7 +58,7 @@ def main():
     lang2prob={}
     for lang in lang2file.keys():
         lang_path = path + lang2file[lang]
-        text_corpus = read_data(path)
+        text_corpus = read_data(lang_path)
         lang2prob[lang]=convert_ngram_prob(text_corpus,3)
     print("ngrams (3) calculated, computing Normalised Jensen-Shannon Divergence")
     for S in lang2file.keys():
